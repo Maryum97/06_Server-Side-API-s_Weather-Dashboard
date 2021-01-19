800ccd3f0df60ff0413cad8990f904255144046c
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-01-19 20:45:44 +0000

    added comments to js file

---
+++ script.js
@@ -0,0 +1,15 @@
+// call classes from search column
+
+// set classes to city-info column // including current date
+
+// set current date using moment.js
+
+// set items to local storage
+
+// add event listener to search button
+
+// get items from local storage to save in search history
+
+// append new elements to the info column so that info of previous city is replaced with that of new city
+
+// set 5 day forecast
